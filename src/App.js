da554138aedb4c6786241ed39cca366701a8d85c
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-12-26 04:09:56 +0530

    add handler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import Handler from './handler';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route component={Handler} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/handler.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const Handler = (props) => {
+  useEffect(() => {
+    window.location = `https://webbrainsmedia.com/tweet2pdf?url=https://twitter.com${props.location.pathname}`;
+  }, []);
+
+  return <div>redirecting...</div>;
+};
+
+export default Handler;
